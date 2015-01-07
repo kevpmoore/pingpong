aae34539e47a37e70e9572db338f1c925ecf91e9
Dean Blacc <dean@marvelapp.com>
2015-01-07 00:01:22 +0000

    fix for leagues and users containing dashes

--- pong/urls.py
+++ pong/urls.py
@@ -21,11 +21,11 @@ urlpatterns = patterns('',
 
     #leagues
     url(r'^api/leagues/$', PlayerLeagueView.as_view()),
-    url(r'^api/leagues/(?P<league_name>[A-Za-z0-9-]+)$', PlayerRankingsView.as_view()),
+    url(r'^api/leagues/(?P<league_name>[A-Za-z0-9-_]+)$', PlayerRankingsView.as_view()),
     url(r'^api/leagues/games/$', LeagueGamesView.as_view()),
-    url(r'^api/leagues/games/(?P<league_name>[A-Za-z0-9-]+)/$', LeagueHistoryView.as_view()),
+    url(r'^api/leagues/games/(?P<league_name>[A-Za-z0-9-_]+)/$', LeagueHistoryView.as_view()),
     url(r'^api/leagues/join/$', LeagueJoinView.as_view()),
-    url(r'^api/leagues/(?P<league_name>[A-Za-z0-9-]+)/(?P<username>[A-Za-z0-9-]+)/$', PlayerView.as_view()),
+    url(r'^api/leagues/(?P<league_name>[A-Za-z0-9-_]+)/(?P<username>[A-Za-z0-9-_]+)/$', PlayerView.as_view()),
 
     #invites
     url(r'^api/invite/new/$', CreatePlayerInviteView.as_view()),
